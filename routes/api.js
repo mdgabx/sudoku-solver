2311555a7ef2119d881b616d6b07da88b86b331e
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-05 20:32:58 +0800

    add: /api/solve solution added

--- routes/api.js
+++ routes/api.js
@@ -10,12 +10,25 @@ module.exports = function (app) {
     .post((req, res) => {
 
       //check the input
-      const { puzzle } = req.body
-        
+      const { puzzle, coordinate, value } = req.body
+ 
+      // sample data in form
+//       {
+//         coordinate : "B3",
+//         puzzle : "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."
+//         value : "3"
+//       }
+
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+
+      const { puzzle } = req.body
+
+      const sudokuSolution = solver.solve(puzzle)
+
+      res.json({ solution: sudokuSolution })
 
     });
 };
