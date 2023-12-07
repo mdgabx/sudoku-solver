78d1411ab941e2cb05b15fe0cd8d5df1f4d97d3a
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-07 21:09:12 +0800

    add: regex test for values to be 1-9

--- routes/api.js
+++ routes/api.js
@@ -5,31 +5,32 @@ const SudokuSolver = require('../controllers/sudoku-solver.js');
 module.exports = function (app) {
   
   let solver = new SudokuSolver();
-
   app.route('/api/check').post((req, res) => {
     const { puzzle, coordinate, value } = req.body;
 
     if (!puzzle || !coordinate || !value) {
-      return res.json({ error: 'Required field(s) missing' });
+        return res.json({ error: 'Required field(s) missing' });
     }
 
     if (puzzle.length !== 81) {
-      return res.json({ error: 'Expected puzzle to be 81 characters long' });
+        return res.json({ error: 'Expected puzzle to be 81 characters long' });
     }
 
+    if (!/^[1-9]$/.test(value)) {
+      return res.json({ error: 'Invalid value' })
+    }
+
+    const row = coordinate.charCodeAt(0) - 'A'.charCodeAt(0);
+    const col = parseInt(coordinate.charAt(1)) - 1;
 
     if (!/^[A-I][1-9]$/.test(coordinate)) {
-        return res.json({ error: 'Invalid coordinate format' });
+      return res.json({ error: 'Invalid coordinate' });
     }
 
     if (!/^[1-9.]{81}$/.test(puzzle)) {
         return res.json({ error: 'Invalid characters in puzzle' });
     }
 
- 
-
-    const row = coordinate.charCodeAt(0) - 'A'.charCodeAt(0);
-    const col = parseInt(coordinate.charAt(1)) - 1;
     const currentValue = parseInt(puzzle[row * 9 + col]);
 
     if (currentValue === parseInt(value)) {
@@ -54,7 +55,6 @@ module.exports = function (app) {
     }
 });
 
-    
   app.route('/api/solve')
     .post((req, res) => {
 
