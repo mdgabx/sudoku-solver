ac877e22a40fe092418396050047f911490acc7a
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-08 22:04:56 +0800

    add: test 3 81 characters not equal

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -19,4 +19,11 @@ suite('Unit Tests', () => {
 
         done();
     })
+
+    test('Logic handles a puzzle string that is not 81 characters in length', function(done) {
+        const sampleInput = "..9..5.1.85.4..432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."
+        assert.notEqual(sampleInput.length, 81, 'Should not equal to 81 characters')
+
+        done();
+    })
 });
