db1a04fda7f3003ca23246799ca1c5e25a909269
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-09 17:37:55 +0800

    add: functional test for /api/solve

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,92 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
+    suite('Functional tests for /api/solve', () => {
 
+        test('Solve a puzzle with valid puzzle string: POST request to /api/solve', (done) => {
+            const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'
+
+            chai.request(server)
+                .post('/api/solve')
+                .send({
+                    puzzle
+                })
+                .end(function(err, res) {
+                    assert.equal(res.status, 200)
+                    assert.property(res.body, 'solution')
+
+                    done()
+                })
+        }).timeout(10000)
+
+        test('Solve a puzzle with missing puzzle string: POST request to /api/solve', (done) => {
+            const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..'
+
+            chai.request(server)
+                .post('/api/solve')
+                .send({
+                    puzzle
+                })
+                .end((err,res) => {
+                    assert.equal(res.status, 200)
+                    assert.property(res.body, 'error')
+                    assert.equal(res.body.error, 'Expected puzzle to be 81 characters long')
+                    
+                    done();
+                })
+        }).timeout(10000)
+
+        test('Solve a puzzle with invalid characters: POST request to /api/solve', (done) => {
+            const puzzle = '82..4..6...16..89...98315.749.157....xx!12.....53..4...96.415..81..7632..3...28.51'
+
+            chai.request(server)
+                .post('/api/solve')
+                .send({
+                    puzzle
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.property(res.body, "error")
+                    assert.equal(res.body.error, "Invalid characters in puzzle")
+
+                    done();
+                })
+        }).timeout(10000)
+
+        test('Solve a puzzle with incorrect length: POST request to /api/solve', (done) => {
+            const puzzle = '..9..5.1.85.4....2432.......'
+
+            chai.request(server)
+                .post('/api/solve')
+                .send({
+                    puzzle
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.property(res.body, "error")
+                    assert.equal(res.body.error, "Expected puzzle to be 81 characters long")
+
+                    done();
+                })
+        }).timeout(10000)
+
+        test('Solve a puzzle that cannot be solved: POST request to /api/solve', (done) => {
+            const puzzle = '53..7....6...4..8.7...327498.........9..83.3...7.4.17.5.268.....9....5....'; // Example unsolvable puzzle
+
+            chai.request(server)
+                .post('/api/solve')
+                .send({ puzzle })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.property(res.body, "error")
+
+                    done()
+                })
+        }).timeout(10000)
+    })
+
+    suite('Functional tests for /api/check', () => {
+        
+    })
 });
 
